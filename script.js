c3f49438bc821502eb13d2bb0a1bc9406e5708b9
Adam Torres <atorres1929@gmail.com>
2020-06-06 05:15:49 -0600

    Overlay has minimal functionality

---
+++ script.js
@@ -0,0 +1,61 @@
+var baseURL = "http://192.168.0.189";
+var refreshRate = 1000;
+var actualTemperatureNozzle = "Pending";
+var targetTemperatureNozzle = "Pending";
+var actualTemperatureBed = "Pending";
+var targetTemperatureBed = "Pending";
+var stateText = "Nothing happening now...";
+
+var interval = setInterval(refresh, refreshRate);
+
+function refresh() {
+  getPrinterInfo();
+  logInfo();
+  displayData();
+}
+
+function logInfo() {
+  console.log(actualTemperatureNozzle);
+  console.log(targetTemperatureNozzle);
+  console.log(actualTemperatureBed);
+  console.log(targetTemperatureBed);
+  console.log(stateText);
+}
+
+function displayData() {
+  $(document).ready(function() {
+    $("#actualTemperatureNozzle").text(actualTemperatureNozzle);
+    $("#targetTemperatureNozzle").text(targetTemperatureNozzle);
+    $("#actualTemperatureBed").text(actualTemperatureBed);
+    $("#targetTemperatureBed").text(targetTemperatureBed);
+    $("#stateText").text(stateText);
+
+  });
+}
+
+function getPrinterInfo() {
+  let printerState = "/api/printer";
+  let printerStateRequest = getRequest(baseURL + printerState);
+  printerStateRequest
+  .then(printerData => setPrinterStatus(printerData))
+  .catch(_ => clearInterval(interval));
+}
+
+function getRequest(url) {
+  return $.get({
+    url: url,
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Api-Key': 'BAEDF577501645078C15126E74809AA6',
+    }
+  });
+}
+
+function setPrinterStatus(printerData) {
+  console.log(printerData);
+  actualTemperatureNozzle = printerData.temperature.tool0.actual;
+  targetTemperatureNozzle = printerData.temperature.tool0.target;
+  actualTemperatureBed = printerData.temperature.bed.actual;
+  targetTemperatureBed = printerData.temperature.bed.target;
+  stateText = printerData.state.text;
+}
